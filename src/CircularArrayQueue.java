ebcf7513064cf838e53df71b4f84024fbe0caba3
Jack-Corbett <31970629+Jack-Corbett@users.noreply.github.com>
2018-03-12 20:38:30 +0000

    Added comments

--- src/CircularArrayQueue.java
+++ src/CircularArrayQueue.java
@@ -1,34 +1,54 @@
 import java.util.NoSuchElementException;
 
+/**
+ * A queue data structure implemented using an array which resizes as the queue grows.
+ */
 public class CircularArrayQueue implements MyQueue {
 
+    // Stores the position of the front of the queue
     private int head;
+    // Stores the position of the end of the queue
     private int tail;
+    // Holds the number of elements currently in the queue
     private int count;
+    // The array which stores the actual values
     private Integer[] array;
 
     /**
-     * Default constructor
+     * Constructor which instantiates the pointers, count and the array to size 10.
      */
     CircularArrayQueue() {
         array = new Integer[10];
         head = tail = count = 0;
     }
 
+    /**
+     * @return The remaining space in the array by subtracting the number of elements from it's size.
+     */
     int getCapacityLeft() {
         return array.length - count;
     }
 
+    /**
+     * Adds an element to the tail of the queue, resizing it if necessary.
+     * @param element The integer to be added.
+     */
     @Override
     public void enqueue(int element) {
         if (array.length == count) {
             resize();
         }
         array[tail] = element;
+        // Use the remainder to allow circular indexing
         tail = (tail + 1) % array.length;
         count++;
     }
 
+    /**
+     * Remove the element from the head of the queue.
+     * @return The element from the queue.
+     * @throws NoSuchElementException If the queue is empty.
+     */
     @Override
     public int dequeue() throws NoSuchElementException {
         if (isEmpty()) {
@@ -37,16 +57,19 @@ public class CircularArrayQueue implements MyQueue {
 
         int element = array[head];
         array[head] = null;
-
+        // Use the remainder to allow circular indexing
         head = (head + 1) % array.length;
         count--;
-
         return element;
     }
 
+    /**
+     * Copy the contents to a new array of twice the size to store more elements.
+     */
     private void resize() {
         Integer[] newArray = new Integer[array.length * 2];
 
+        // Copy all of the elements from the old array across
         for (int i = 0; i < count; i++) {
             newArray[i] = array[head];
             head = (head + 1) % array.length;
@@ -54,14 +77,21 @@ public class CircularArrayQueue implements MyQueue {
 
         head = 0;
         tail = count;
+        // Change the array reference to the new larger array
         array = newArray;
     }
 
+    /**
+     * @return The number of items in the queue
+     */
     @Override
     public int noItems() {
         return count;
     }
 
+    /**
+     * @return True if there are no elements in the queue, false otherwise.
+     */
     @Override
     public boolean isEmpty() {
         return (count == 0);
--- src/CircularArrayRing.java
+++ src/CircularArrayRing.java
@@ -1,17 +1,24 @@
 import java.util.AbstractCollection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 import static java.lang.StrictMath.floorMod;
 
+/**
+ * A data structure that only remembers the last n elements (default is 10) which is circular to make optimal use of
+ * memory.
+ * @param <E> sets the type
+ */
 public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<E> {
 
+    // Stores a pointer to the next position to fill
     private int tail;
+    // Keeps track of the number of elements stored
     private int count;
+    // Holds the actual objects
     private E[] array;
 
     /**
-     * Default constructor
+     * Default constructor which initialises the tail, count and array to the default size of 10.
      */
     CircularArrayRing() {
         tail = count = 0;
@@ -19,19 +26,28 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
     }
 
     /**
-     *
-     * @param size
+     * Constructor that initialises the tail and count, setting the length of the array to the passed value.
+     * @param size The desired size of the ring.
      */
     CircularArrayRing(int size) {
         tail = count = 0;
         array = (E[]) new Object[size];
     }
 
+    /**
+     * Adds an elements to the circular array ring, updating the tail and count.
+     * @param e Element to add to the data structure
+     * @return Always returns true for this data structure as you can always add an element
+     * even if it is full as it will overwrite another.
+     */
     @Override
     public boolean add(E e) {
         array[tail] = e;
+
+        // Use mod to ensure the tail stays within the array bounds (ie set to 0 when it reaches the array size)
         tail = floorMod((tail + 1), array.length);
 
+        // Only increment count if the array hasn't yet been filled
         if (count < array.length) {
             count++;
         }
@@ -39,10 +55,21 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
         return true;
     }
 
+    /**
+     * Get an element from the circular array ring.
+     * @param index The position of the element to access.
+     * @return The fetched object.
+     * @throws IndexOutOfBoundsException If there is no element in that position or the index
+     * given was outside the array bounds.
+     */
     @Override
     public E get(int index) throws IndexOutOfBoundsException {
         try {
+            /* If there are elements in the array AND the index given is less than the number of elements
+            AND the index is 0 or greater then fetch the element.
+             */
             if (count > 0 && index < count && index >= 0) {
+                // Use mod for the circular behaviour, keeping elements in the correct order
                 return array[floorMod((tail - (index + 1)), array.length)];
             } else {
                 throw new IndexOutOfBoundsException();
@@ -52,22 +79,40 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
         }
     }
 
+    /**
+     * An iterator to loop through each element in the circular array ring, backwards from the
+     * most recently added.
+     * @return The iterator object.
+     */
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
+
             private int itCount;
 
+            // Constructor initialises the iterator count to 0
             {
                 itCount = 0;
             }
 
+            /**
+             * Checks if there is an object in the next position by comparing the iterator count with the length
+             * of the array and the element count.
+             * @return True if there is an object to iterate to.
+             * False if there is no object OR we have iterated through the entire array.
+             */
             @Override
             public boolean hasNext() {
                 return size() == array.length && itCount < array.length - 1 || itCount < size();
             }
 
+            /**
+             * Fetches the next object from the data structure.
+             * @return The fetched object.
+             */
             @Override
             public E next() {
+                // Check there is an object in the next position and fetch it
                 if(hasNext()) {
                     return get(itCount++);
                 } else {
@@ -75,6 +120,9 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
                 }
             }
 
+            /**
+             * Not implemented for this data structure.
+             */
             @Override
             public void remove() {
                 throw new UnsupportedOperationException();
@@ -82,6 +130,9 @@ public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<
         };
     }
 
+    /**
+     * @return The number of elements in the array, once it is full this remains constant.
+     */
     @Override
     public int size() {
         return count;
